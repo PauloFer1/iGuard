a9743949b104c71bc4c28bf954c7f490c53c72d0
Paulo Fernandes <paulofernandes@tarambola.pt>
2014-08-25 15:14:30 +0100

    Initial Commit

---
+++ iGuard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iGuard
+//
+//  Created by Paulo Fernandes on 25/08/14.
+//  Copyright (c) 2014 Paulo Fernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iGuard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iGuard
+//
+//  Created by Paulo Fernandes on 25/08/14.
+//  Copyright (c) 2014 Paulo Fernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
